72e6d3a52e61023fcd7d05e2e31626c8572d435d
Canthar90 <d.cieslak90@gmail.com>
2022-12-30 13:21:30 +0100

    Added links, specyfy language, limit of news numbers

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import requests
 import os
 from dotenv import load_dotenv
 from send_email import send_email
+import datetime as dt
 
 
 load_dotenv("global.env")
 API_KEY = os.getenv("API_KEY")
 
+today = dt.date.today()
+month_ago = today - dt.timedelta(days=30)
+month_ago = month_ago.strftime("%Y-%m-%d")
+
 
 url = f"https://newsapi.org/v2/everything?q=tesla&\
-from=2022-11-30&sortBy=publishedAt&apiKey={API_KEY}" 
+from={month_ago}&sortBy=publishedAt&apiKey={API_KEY}&language=en" 
 
 # Make request
 request = requests.get(url)
@@ -17,11 +22,12 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-message = "Recent news on tesla are: \n\n\n"
+message = "Subject: Recent news on tesla are:\n"
 
-for article in content["articles"]:
-    if article["titile"] is not None:
-        message += f"""{article["title"]} \n {article["description"]}\n\n"""
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        message += f"""{article["title"]} \n \
+            {article["description"]}\n {article["url"]} \n\n"""
     
 message = message.encode("utf-8")    
 send_email(message)
